b668a7c659996838743649718015c62f1a7cb1c9
Michael Gerver <mgerver@ufl.edu>
2020-02-06 21:43:03 -0500

    Add orderPizza function to Javascript file

--- main.js
+++ main.js
@@ -5,4 +5,11 @@ var pizza = {
     addToppings: function() {
         console.log('Please add pepperoni and sausage');
     }
-}
+};
+
+function orderPizza() {
+    console.log(pizza.crust);
+    console.log(pizza.cheese);
+    console.log(pizza.sauce);
+    console.log(pizza.addToppings());
+};
